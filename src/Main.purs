ca170e0f8716c6a9f8193a4ee50e027acd7c69e9
David Richey <darichey1@gmail.com>
2022-09-03 15:27:29 -0500

    Implement lambda parsing

--- src/Main.purs
+++ src/Main.purs
@@ -2,16 +2,26 @@ module Main where
 
 import Prelude
 
+import Control.Alt ((<|>))
+import Control.Lazy (fix)
+import Data.Array.NonEmpty (toArray)
+import Data.Either (Either(..))
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
+import Data.String.CodeUnits (fromCharArray)
 import Effect (Effect)
 import Effect.Class.Console (logShow)
+import Parsing (Parser, fail, runParser)
+import Parsing.Combinators (between, chainl1)
+import Parsing.Combinators.Array (many1)
+import Parsing.String (char)
+import Parsing.String.Basic (alphaNum, skipSpaces, space)
 
 main :: Effect Unit
 main = do
-  logShow $ reduce term
-    where
-    term = App (Abs "x" (Var "x")) (Abs "y" (Var "y"))
+  case runParser "(\\x.x) x" term of
+    Left e -> logShow e
+    Right t -> logShow $ reduce t
 
 data Term = Var String | Abs String Term | App Term Term
 
@@ -31,3 +41,18 @@ reduce (App e1 e2) = App e1' e2
   where
   e1' = reduce e1
 reduce x = x
+
+varString :: Parser String String
+varString = fromCharArray <$> toArray <$> many1 alphaNum
+
+var :: Parser String Term
+var = Var <$> varString
+
+abs :: Parser String Term -> Parser String Term
+abs p = Abs <$> (char '\\' *> skipSpaces *> varString <* skipSpaces) <*> (char '.' *> skipSpaces *> p)
+
+nonApp :: Parser String Term -> Parser String Term
+nonApp p = (between (char '(') (char ')') p) <|> abs p <|> var
+
+term :: Parser String Term
+term = fix \p -> skipSpaces *> chainl1 (nonApp p) (space *> pure App)
