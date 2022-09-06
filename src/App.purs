08aa8b0a3ca1bcda6a575be39b1b98bf4fb2a4a2
David Richey <darichey1@gmail.com>
2022-09-05 20:58:21 -0500

    Rework evaluation gui

--- src/App.purs
+++ src/App.purs
@@ -2,14 +2,15 @@ module App (appComponent) where
 
 import Prelude
 
-import Data.Either (Either(..))
-import Data.List (List(..), filter, reverse, toUnfoldable, (:))
+import Data.Array (catMaybes, length, mapWithIndex, snoc, toUnfoldable, (..))
+import Data.Either (Either(..), hush)
+import Data.List (List(..), (:))
+import Data.Traversable (for)
 import Data.Tuple.Nested ((/\))
 import Effect.Aff.Class (class MonadAff)
-import Effect.Class.Console (debug)
-import Effect.Unsafe (unsafePerformEffect)
 import Halogen (PropName(..))
 import Halogen as H
+import Halogen.HTML (HTML)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
@@ -18,60 +19,97 @@ import Lambda.Model (Term(..))
 import Lambda.Parse (term)
 import Lambda.Pretty (prettyPrint)
 import Lambda.Reduce (reduce)
-import Parsing (parseErrorMessage, runParser)
+import Parsing (ParseError, parseErrorMessage, runParser)
+import Type.Proxy (Proxy(..))
+
+type CellState = { name :: String, value :: String }
 
 type Binding = { name :: String, value :: Term }
 
 type Environment = List Binding
-
-deleteBinding :: String -> Environment -> Environment
-deleteBinding name = filter (\b -> b.name /= name)
-
-putBinding :: Binding -> Environment -> Environment
-putBinding binding environment = binding : (deleteBinding binding.name environment)
 
 withEnv :: Environment -> Term -> Term
 withEnv Nil t = t
 withEnv (b : bs) t = withEnv bs (App (Abs b.name t) b.value)
 
 appComponent :: forall query input output m. MonadAff m => H.Component query input output m
-appComponent = Hooks.component \_ _ -> Hooks.do
-  environment /\ environmentId <- Hooks.useState Nil
-
-  name /\ nameId <- Hooks.useState ""
+appComponent = Hooks.component \{ slotToken } _ -> Hooks.do
   expression /\ expressionId <- Hooks.useState ""
+  cells /\ cellsId <- Hooks.useState []
+  environment /\ environmentId <- Hooks.useState Nil
 
   Hooks.pure do
     HH.div
-      [ twclass "inline-flex flex-col" ]
-      [ HH.text "Environment"
-      , HH.div_ $ toUnfoldable $ reverse $ environment <#> (\{ name, value } -> HH.div_ [ HH.text $ name <> " = " <> prettyPrint value ])
-      , HH.text "Name"
-      , HH.input
-          [ HP.value name
-          , HE.onValueInput \input -> Hooks.put nameId input
-          , twclass "border-black border-2"
+      [ twclass "grid grid-cols-12 grid-rows-3" ]
+      [ HH.div [ twclass "col-span-2" ] [ HH.text "let" ]
+      , HH.div
+          [ twclass "col-span-10" ]
+          [ HH.div_ $ mapWithIndex (\i cell -> HH.slot _cell i cellComponent cell (const $ pure unit)) cells
+          , HH.button
+              [ HE.onClick \_ -> Hooks.modify_ cellsId (flip snoc { name: "", value: "" }) ]
+              [ HH.text "Add cell" ]
           ]
-      , HH.text "Expression"
-      , HH.input
-          [ HP.value expression
-          , HE.onValueInput \input -> Hooks.put expressionId input
-          , twclass "border-black border-2"
+      , HH.div [ twclass "col-span-2" ] [ HH.text "in" ]
+      , HH.div
+          [ twclass "col-span-10" ]
+          [ HH.input
+              [ twclass "border-2 border-black"
+              , HP.value expression
+              , HE.onValueInput \s -> do
+                  env <- (for (0 .. (length cells - 1)) \i -> Hooks.request slotToken _cell i GetTerm)
+                  Hooks.put environmentId (toUnfoldable $ catMaybes env)
+                  Hooks.put expressionId s
+              ]
           ]
-      , case runParser expression term of
-          Left error -> HH.div
-            [ twclass "text-red-400" ]
-            [ HH.text $ parseErrorMessage error ]
-          Right t -> HH.div
-            [ twclass "inline-flex flex-col" ]
-            [ HH.div_ [ HH.text $ "Input: " <> prettyPrint t ]
-            , HH.div_ [ HH.text $ "w/ env: " <> (prettyPrint $ withEnv environment t) ]
-            , HH.div_ [ HH.text $ "Reduced: " <> (prettyPrint $ reduce $ withEnv environment t) ]
-            , HH.button
-                [ HE.onClick \_ -> Hooks.modify_ environmentId (putBinding { name, value: t }) ]
-                [ HH.text "Add to env" ]
-            ]
+      , HH.div
+          [ twclass "col-span-full" ]
+          [ parseResultDiv (runParser expression term) \t ->
+              let
+                reduced = reduce $ withEnv environment t
+              in
+                HH.div_ [ HH.text $ prettyPrint reduced ]
+          ]
       ]
+
+data Query a = GetTerm (Binding -> a)
+
+_cell = Proxy :: Proxy "cell"
+
+cellComponent :: forall output m. MonadAff m => H.Component Query CellState output m
+cellComponent = Hooks.component \{ queryToken } { name: iName, value: iValue } -> Hooks.do
+  name /\ nameId <- Hooks.useState iName
+  value /\ valueId <- Hooks.useState iValue
+
+  let parsed = runParser value term
+
+  Hooks.useQuery queryToken case _ of
+    GetTerm reply -> pure $ reply <$> { name, value: _ } <$> hush parsed
+
+  Hooks.pure do
+    HH.div
+      [ twclass "border-2 border-black p-2 mb-2" ]
+      [ HH.div_
+          [ HH.input
+              [ twclass "border-2 border-black"
+              , HP.value name
+              , HE.onValueInput $ Hooks.put nameId
+              ]
+          , HH.text " = "
+          , HH.input
+              [ twclass "border-2 border-black"
+              , HP.value value
+              , HE.onValueInput $ Hooks.put valueId
+              ]
+          ]
+      , parseResultDiv parsed (const (HH.div_ []))
+      ]
+
+parseResultDiv :: forall w i. Either ParseError Term -> (Term -> HTML w i) -> HTML w i
+parseResultDiv parsed f = case parsed of
+  Left error -> HH.div
+    [ twclass "text-red-400" ]
+    [ HH.text $ parseErrorMessage error ]
+  Right t -> f t
 
 twclass :: forall r i. String -> HP.IProp (class :: String | r) i
 twclass = HP.prop (PropName "className")
